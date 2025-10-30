8f01bc41486e7f7ab4360eae10c1011908357847
GunjanTiwari-web <gunjantiwari.gcsb@gmail.com>
2025-10-30 19:41:15 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/json-tree-visualizer/", // 
+  base: "/json-tree-visualizer/", // ✅ Correct placement
 });
